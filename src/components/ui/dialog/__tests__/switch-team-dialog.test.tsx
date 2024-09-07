633cca6994d65a1321aac2b03990431a5b544188
Sandushan8 <dewpurai@gmail.com>
2024-09-07 21:22:07 +0530

    Spacing inside the tests code

--- src/components/ui/dialog/__tests__/switch-team-dialog.test.tsx
+++ src/components/ui/dialog/__tests__/switch-team-dialog.test.tsx
@@ -4,6 +4,7 @@ import {
   SwitchTeamDialogProps,
 } from '../switch-team-dialog/switch-team-dialog';
 import { useForm } from 'react-hook-form';
+
 const mockStorage = vi.fn(() => {
   let store: Record<string, string> = {};
   return {
@@ -19,16 +20,20 @@ const mockStorage = vi.fn(() => {
     }),
   };
 })();
+
 beforeAll(() => {
   global.sessionStorage = mockStorage as unknown as Storage;
 });
+
 afterAll(() => {
   global.sessionStorage = window.sessionStorage;
 });
+
 const options = [
   { label: 'Team 1', value: 'team1' },
   { label: 'Team 2', value: 'team2' },
 ];
+
 const TestComponent = (props: Partial<SwitchTeamDialogProps>) => {
   const { register, watch } = useForm<{
     teamId: string;
@@ -45,16 +50,20 @@ const TestComponent = (props: Partial<SwitchTeamDialogProps>) => {
     currentTeam: 'team1',
     ...props,
   };
+
   return <SwtichTeamDialog {...defaultProps} />;
 };
+
 test('should render dialog with teams and switch the team', async () => {
   const onClose = vi.fn();
   rtlRender(<TestComponent onClose={onClose} />);
+
   expect(screen.getByText('Switch Team')).toBeInTheDocument();
   expect(screen.getByDisplayValue('Team 1')).toBeInTheDocument();
   fireEvent.change(screen.getByRole('combobox'), {
     target: { value: 'team2' },
   });
+
   expect(screen.getByDisplayValue('Team 2')).toBeInTheDocument();
   fireEvent.click(screen.getByText('Confirm'));
   await waitFor(() => {
@@ -62,9 +71,12 @@ test('should render dialog with teams and switch the team', async () => {
   });
   expect(onClose).toHaveBeenCalled();
 });
+
 test('should show "No teams found" when there are no teams', () => {
   rtlRender(<TestComponent options={[]} />);
+
   expect(screen.getByText('No teams found')).toBeInTheDocument();
+
   const confirmButton = screen.getByRole('button', { name: /Confirm/i });
   expect(confirmButton).toBeInTheDocument();
   expect(confirmButton).toBeDisabled();
--- src/components/ui/form/__tests__/multi-select.test.tsx
+++ src/components/ui/form/__tests__/multi-select.test.tsx
@@ -1,14 +1,17 @@
 import { Control, useForm } from 'react-hook-form';
 import { rtlRender, screen, fireEvent } from '@/testing/test-utils';
 import { MultiSelect, MultiSelectFieldProps } from '../multi-select';
+
 type FormValues = {
   multiSelect: string[];
 };
+
 const options = [
   { value: 'option1', label: 'Option 1' },
   { value: 'option2', label: 'Option 2' },
   { value: 'option3', label: 'Option 3' },
 ];
+
 const TestComponent = (props: Partial<MultiSelectFieldProps>) => {
   const { control } = useForm<FormValues>({
     defaultValues: {
@@ -24,33 +27,45 @@ const TestComponent = (props: Partial<MultiSelectFieldProps>) => {
   };
   return <MultiSelect {...defaultProps} />;
 };
+
 test('should render and select multiple options in MultiSelect component', async () => {
   rtlRender(<TestComponent />);
+
   const selectInput = screen.getByRole('combobox');
   fireEvent.keyDown(selectInput, { key: 'ArrowDown' });
   const option1 = await screen.findByText('Option 1');
   fireEvent.click(option1);
+
   fireEvent.keyDown(selectInput, { key: 'ArrowDown' });
   const option2 = await screen.findByText('Option 2');
   fireEvent.click(option2);
+
   expect(screen.getByText('Option 1')).toBeInTheDocument();
   expect(screen.getByText('Option 2')).toBeInTheDocument();
 });
+
 test('should show No options found when there are no options', async () => {
   rtlRender(<TestComponent options={[]} />);
+
   const selectInput = screen.getByRole('combobox');
+
   fireEvent.keyDown(selectInput, { key: 'ArrowDown' });
   expect(screen.getByText('No options')).toBeInTheDocument();
 });
+
 test('should be able to remove selected elements', async () => {
   rtlRender(<TestComponent />);
+
   const selectInput = screen.getByRole('combobox');
+
   fireEvent.keyDown(selectInput, { key: 'ArrowDown' });
   const option1 = await screen.findByText('Option 1');
   fireEvent.click(option1);
+
   fireEvent.keyDown(selectInput, { key: 'ArrowDown' });
   const option2 = await screen.findByText('Option 2');
   fireEvent.click(option2);
+
   expect(screen.getByText('Option 1')).toBeInTheDocument();
   expect(screen.getByText('Option 2')).toBeInTheDocument();
   fireEvent.click(screen.getByLabelText('Remove Option 1'));
